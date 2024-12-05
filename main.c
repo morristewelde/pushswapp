acfa19e6353e7575bea5e765340b155183593e8d
Morris S. TEWELDE <132311057+morristewelde@users.noreply.github.com>
2024-12-06 00:18:20 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtewelde <mtewelde@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/05 12:54:32 by mtewelde          #+#    #+#             */
+/*   Updated: 2024/10/23 00:11:56 by mtewelde         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	push_swap(t_stack **a)
+{
+	t_stack	**b;
+
+	b = malloc(sizeof(t_stack *));
+	if (!b)
+		return ;
+	*b = NULL;
+	if (ft_stack_len(*a) == 2)
+		sa(a);
+	else if (ft_stack_len(*a) == 3)
+		sort_three(a);
+	else
+		sort_big(a, b);
+	free_stack(b);
+}
+
+int	main(int ac, char *av[])
+{
+	t_stack	**a;
+
+	if (ac == 1)
+		return (0);
+	if (ac > 1)
+	{
+		if (valid_av(av))
+			ft_error();
+		a = initialize(av);
+		if (!a)
+			ft_error();
+		if (check_sorted(a))
+		{
+			free_stack(a);
+			return (0);
+		}
+		else
+			push_swap(a);
+		free_stack(a);
+	}
+}
